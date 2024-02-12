4e740f69185a875c0b3a9ccea3ebed504b492719
Ricardo <ricardo4q@gmail.com>
2024-02-11 21:13:39 -0300

    FirstCommit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.getElementById('termo').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        pesquisar();
+    }
+});
+
+function pesquisar() {
+    var selectSite = document.getElementById('select-site');
+    var site = selectSite.options[selectSite.selectedIndex].value;
+    var termo = encodeURIComponent(document.getElementById('termo').value);
+    var url = '';
+
+    switch (site) {
+        case 'google':
+            url = 'https://www.google.com/search?q=' + termo;
+            break;
+        case 'youtube':
+            url = 'https://www.youtube.com/results?search_query=' + termo;
+            break;
+        case 'spotify':
+            url = 'https://open.spotify.com/search/' + termo;
+            break;
+        case 'instagram':
+            url = 'https://www.instagram.com/explore/tags/' + termo;
+            break;
+        default:
+            break;
+    }
+
+    window.open(url, '_blank');
+}
+
+function alterarTema() {
+    var selectSite = document.getElementById('select-site');
+    var site = selectSite.options[selectSite.selectedIndex].value;
+    var body = document.body;
+
+    body.setAttribute('data-site', site);
+}
